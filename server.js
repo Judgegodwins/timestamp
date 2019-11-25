bfb82f361aa68ed9077809237c7e3f87173e114e
Glitch (destiny-nutria) <none>
2019-11-25 13:38:50 +0000

    🐑🐧 Updated with Glitch

--- server.js
+++ server.js
@@ -23,43 +23,29 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
-app.get('/api/timestamp/', (req, res) => {
-  res.json({ unix: Date.now(), UTC: Date() })
-})
-app.get('/api/timestamp/:date', (req, res, done) => {
-  var dateString = req.params.date;
-  
-  if(/\d{5}/.test(dateString)) {
-    let dateInt = parseInt(dateString);
-    
-    let date= new Date(dateInt);
-    
-    res.json({unix: dateInt, UTC: date.toUTCString()});
-  }
-  let dateObject = new Date(dateString);
-  
-  if(dateObject.toString() =='Invalid Date') {
-    res.json({'error': 'Invalid Date'})
-  } else {
-    
-    res.json({ unix: dateObject.valueOf(), UTC: dateObject.toUTCString() });
-    
-  }
-  
-})
-function sendJson(a, b, c, cb) {
-  var time, unixTime
-  try {
-    time = new Date(a, b, c).toUTCString();
-    unixTime = new Date(a, b, c).getTime();
-  }
-  catch(err) {
-    cb(err, null)
-    return
-  }
-  cb(null, [unixTime, time])
-}
+app.get("/api/timestamp/", (req, res) => {
+  res.json({ unix: Date.now(), utc: Date() });
+});
 
+app.get("/api/timestamp/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+
+  //A 4 digit number is a valid ISO-8601 for the beginning of that year
+  //5 digits or more must be a unix time, until we reach a year 10,000 problem
+  if (/\d{5,}/.test(dateString)) {
+    dateInt = parseInt(dateString);
+    //Date regards numbers as unix timestamps, strings are processed differently
+    res.json({ unix: dateString, utc: new Date(dateInt).toUTCString() });
+  }
+
+  let dateObject = new Date(dateString);
+
+  if (dateObject.toString() === "Invalid Date") {
+    res.json({ error: "Invaid Date" });
+  } else {
+    res.json({ unix: dateObject.valueOf(), utc: dateObject.toUTCString() });
+  }
+});
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
